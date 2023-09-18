3d0ccf7820d0b6ced364b33d07490c0c9c478b12
luizAFS23 <luizafs.1500@gmail.com>
2023-09-18 19:17:58 -0300

    changes

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('#container');
+
+const btn = document.querySelectorAll('button')
+
+
+btn.forEach((button) =>{
+    button.addEventListener('click', () => {
+        alert(button.id);
+    })
+})
